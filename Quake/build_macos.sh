2f52b536e6d4dfbcc9bf35169ec3f386a6b78c16
Tom Kidd <tomkidd@gmail.com>
2021-10-02 20:56:07 -0500

    Update build_macos.sh

--- Quake/build_macos.sh
+++ Quake/build_macos.sh
@@ -122,23 +122,95 @@ echo -e "${PLIST}" > "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
 
 echo "bundle done."
 
-# ****************************************************************************************
-# identity as specified in Keychain
-SIGNING_IDENTITY="Developer ID Application: Your Name (XXXXXXXXX)"
+if [ "$1" == "notarize" ]; then
+	# user-specific values
+	# specify the actual values in a separate file called make-macosx-values.sh
 
-ASC_USERNAME="your@apple.id"
+    # ****************************************************************************************
+    # identity as specified in Keychain
+    SIGNING_IDENTITY="Developer ID Application: Your Name (XXXXXXXXX)"
 
-# signing password is app-specific (https://appleid.apple.com/account/manage) and stored in Keychain (as "notarize-app" in this case)
-ASC_PASSWORD="@keychain:notarize-app"
+    ASC_USERNAME="your@apple.id"
 
-# ProviderShortname can be found with
-# xcrun altool --list-providers -u your@apple.id -p "@keychain:notarize-app"
-ASC_PROVIDER="XXXXXXXXX"
-# ****************************************************************************************
+    # signing password is app-specific (https://appleid.apple.com/account/manage) and stored in Keychain (as "notarize-app" in this case)
+    ASC_PASSWORD="@keychain:notarize-app"
 
-source make-macosx-values.sh
+    # ProviderShortname can be found with
+    # xcrun altool --list-providers -u your@apple.id -p "@keychain:notarize-app"
+    ASC_PROVIDER="XXXXXXXXX"
+    # ****************************************************************************************
 
-# sign the resulting app bundle
-echo "signing..."
-# codesign --force --options runtime --deep --entitlements "${ENTITLEMENTS_FILE}" --sign "${SIGNING_IDENTITY}" ${RELEASE_LOCATION}/${RELEASE_BUILD}	# sign the resulting app bundle
-codesign --force --deep --sign "${SIGNING_IDENTITY}" ${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}
+    source make-macosx-values.sh
+
+    # release build location
+	RELEASE_LOCATION="build/release-darwin-universal2"
+
+	# release build name
+	RELEASE_BUILD="ioquake3.app"
+
+	# Pre-notarized zip file (not what is shipped)
+	PRE_NOTARIZED_ZIP="vkquake_prenotarized.zip"
+
+	# Post-notarized zip file (shipped)
+	POST_NOTARIZED_ZIP="vkquake_notarized.zip"
+
+    BUNDLE_ID="com.macsourceports.vkquake3"
+
+    # sign the resulting app bundle
+    echo "signing..."
+    # codesign --force --options runtime --deep --entitlements "${ENTITLEMENTS_FILE}" --sign "${SIGNING_IDENTITY}" ${RELEASE_LOCATION}/${RELEASE_BUILD}	# sign the resulting app bundle
+    codesign --force --options runtime --deep --sign "${SIGNING_IDENTITY}" ${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}
+
+    cd ${BUILT_PRODUCTS_DIR}
+
+	# notarize app
+	# script taken from https://github.com/rednoah/notarize-app
+
+	# create the zip to send to the notarization service
+	echo "zipping..."
+	ditto -c -k --sequesterRsrc --keepParent ${WRAPPER_NAME} ${PRE_NOTARIZED_ZIP}
+
+	# create temporary files
+	NOTARIZE_APP_LOG=$(mktemp -t notarize-app)
+	NOTARIZE_INFO_LOG=$(mktemp -t notarize-info)
+
+	# delete temporary files on exit
+	function finish {
+		rm "$NOTARIZE_APP_LOG" "$NOTARIZE_INFO_LOG"
+	}
+	trap finish EXIT
+
+	echo "submitting..."
+	# submit app for notarization
+	if xcrun altool --notarize-app --primary-bundle-id "$BUNDLE_ID" --asc-provider "$ASC_PROVIDER" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" -f "$PRE_NOTARIZED_ZIP" > "$NOTARIZE_APP_LOG" 2>&1; then
+		cat "$NOTARIZE_APP_LOG"
+		RequestUUID=$(awk -F ' = ' '/RequestUUID/ {print $2}' "$NOTARIZE_APP_LOG")
+
+		# check status periodically
+		while sleep 60 && date; do
+			# check notarization status
+			if xcrun altool --notarization-info "$RequestUUID" --asc-provider "$ASC_PROVIDER" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" > "$NOTARIZE_INFO_LOG" 2>&1; then
+				cat "$NOTARIZE_INFO_LOG"
+
+				# once notarization is complete, run stapler and exit
+				if ! grep -q "Status: in progress" "$NOTARIZE_INFO_LOG"; then
+					xcrun stapler staple "$WRAPPER_NAME"
+					break
+				fi
+			else
+				cat "$NOTARIZE_INFO_LOG" 1>&2
+				exit 1
+			fi
+		done
+	else
+		cat "$NOTARIZE_APP_LOG" 1>&2
+		exit 1
+	fi
+
+	echo "notarized"
+	echo "zipping notarized..."
+
+	ditto -c -k --sequesterRsrc --keepParent ${WRAPPER_NAME} ${POST_NOTARIZED_ZIP}
+
+	echo "done. ${POST_NOTARIZED_ZIP} contains notarized ${WRAPPER_NAME} build."
+fi
